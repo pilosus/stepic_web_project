eebcbb13e550ddeb88ab154d142a8a41ca9c0cdb
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-15 18:26:21 +0200

    nginx config minor changes

--- init.sh
+++ init.sh
@@ -8,8 +8,8 @@
 
 # it's better to run script with sudo
 # sudo bash /home/box/web/init.sh
-﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-/etc/init.d/nginx restart
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
 
 
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
