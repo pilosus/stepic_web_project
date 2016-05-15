a262c3594e12e1fb43991356085503917a9a21ac
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-15 17:25:18 +0200

    regexp changes in nginx config

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
 ### Создание проекта в рамках курса на stepic.org
 
 # $ mkdir -p /home/box/web
-# $ git clone https://github.com/your_account/stepic_web_project.git /home/box/web
+# $ git clone https://github.com/pilosus/stepic_web_project.git /home/box/web
 # $ bash /home/box/web/init.sh
 
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
+# it's better to run script with sudo
+# sudo bash /home/box/web/init.sh
+﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
+
+
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
