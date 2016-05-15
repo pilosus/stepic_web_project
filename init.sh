250573ec0b64eab0d347f5a3c8088e769e477b3f
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-15 17:09:30 +0200

    step 12

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 ### Создание проекта в рамках курса на stepic.org
 
+# $ mkdir -p /home/box/web
 # $ git clone https://github.com/your_account/stepic_web_project.git /home/box/web
 # $ bash /home/box/web/init.sh
-echo "it works!"
+
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
