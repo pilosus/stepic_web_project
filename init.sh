ea42525efe86f65fa253dd2079da82f290572fd0
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-15 18:50:36 +0200

    Step 10 complete

--- init.sh
+++ init.sh
@@ -11,6 +11,5 @@
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+# in /etc/nginx/sites-enabled/default
+# first two comment lines with listen 80 /server_default
