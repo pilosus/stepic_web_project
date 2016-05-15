1c0c59e7d34142526e7f88e7043a2241f688408b
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-15 16:57:30 +0200

    initial commit

---
+++ init.sh
@@ -0,0 +1,5 @@
+### Создание проекта в рамках курса на stepic.org
+
+# $ git clone https://github.com/your_account/stepic_web_project.git /home/box/web
+# $ bash /home/box/web/init.sh
+echo "it works!"
