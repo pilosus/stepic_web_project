5aab07a8f363cb5c7861564ddf6cd63565d7ff56
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-16 13:39:09 +0200

    new nginx config

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test, name='test'),        
-    url(r'^login/$', views.test),
-    url(r'^signup/$', views.test),
-    url(r'^ask/$', views.test),
-    url(r'^popular/$', views.test),
-    url(r'^new/$', views.test),
+    url(r'^login/.*$', views.test),
+    url(r'^signup/.*$', views.test),
+    url(r'^ask/.*$', views.test),
+    url(r'^popular/.*$', views.test),
+    url(r'^new/.*$', views.test),
     url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),    
 ]
