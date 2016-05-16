127b24c024a24ec20e2773f2241d5e36d40c9a32
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-16 14:25:53 +0200

    Lesson 0 completed

--- init.sh
+++ init.sh
@@ -20,5 +20,6 @@ sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django-gunicorn.conf  /etc/gunicorn.d/django-gunicorn.conf
 
 # run gunicorn server
+cd /home/box/web/ask
 sudo gunicorn -с /etc/gunicorn.d/hello.py hello:app
 sudo gunicorn -с /etc/gunicorn.d/django-gunicorn.conf ask.wsgi:application
