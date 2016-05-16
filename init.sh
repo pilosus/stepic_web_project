11aaf6fcfb7f2daa0d22dbc0dbde9a2a472d0245
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-16 13:56:38 +0200

    gunicorn config fixed

--- init.sh
+++ init.sh
@@ -6,13 +6,19 @@
 # if git files changed locally, stash or reset:
 # $ git reset --hard
 
-# it's better to run script with sudo
-# sudo bash /home/box/web/init.sh
+# create symbolic link to a new nginx config
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+
+# restart nginx
 sudo /etc/init.d/nginx restart
 
 # in /etc/nginx/sites-enabled/default
 # first two comment lines with listen 80 /server_default
 
+# creare symbolic links to gunicorn configs
 sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/django-gunicorn.conf  /etc/gunicorn.d/django-gunicorn.conf
+
+# run gunicorn server
 gunicorn -с /etc/gunicorn.d/hello.py hello:app
+gunicorn -с /etc/gunicorn.d/django-gunicorn.conf ask.wsgi:application
