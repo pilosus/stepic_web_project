083251e911b94b96b181e446946aad34be1b55e7
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-17 14:32:49 +0200

    init script minor fix

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@
 
 # create symbolic link to a new nginx config
 sudo -s ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/django.conf
-sudo -s rm /etc/nginx/sites-enabled/default.conf
+sudo -s rm /etc/nginx/sites-enabled/default
 
 # restart nginx
 sudo -s /etc/init.d/nginx restart
