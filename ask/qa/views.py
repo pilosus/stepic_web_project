33dcb206ad40d5deb600aefe3676b15c98fa3b18
Vitaly R. Samigullin <vrs@pilosus.org>
2016-05-17 11:44:46 +0200

    Lesson 2.2 Template basics

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
 from django.http import HttpResponse 
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK')
+    context = {'var1': 1, 'var2': 2}
+    return render(request, 'qa/index.html', context)
+    #return HttpResponse('OK')
 
 def question(request, question_id):
     return HttpResponse("You have reached question #%s" % question_id)
